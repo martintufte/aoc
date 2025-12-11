15a76876c45076005fc955957249d8422bf62021
martintufte <martingudahltufte@hotmail.com>
2025-12-11 09:47:13 +0100

    day 1, part 1

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Advent of code 2025 in rust
 
 First time doing advent of code
+
 First time writing rust code
--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,12 +1,28 @@
 aoc::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u64> {
-    println!("{input}");
-    None
+pub fn part_one(input: &str) -> Option<u64> {    
+    // Initialize values
+    let mut count: u64 = 0;
+    let mut value: i32 = 50;
+
+    for line in input.lines() {
+        let n: i32 = line[1..].parse().unwrap();
+        if line.starts_with("R") {
+            value += n;
+        } else if line.starts_with("L") {
+            value -= n;
+        }
+        // Add value if modulo is zero
+        if value % 100 == 0 {
+            count += 1;
+        }
+    }
+
+    Some(count)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    println!("{input}");
+pub fn part_two(_input: &str) -> Option<u64> {
+    // println!("{input}");
     None
 }
 
@@ -17,7 +33,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&aoc::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
