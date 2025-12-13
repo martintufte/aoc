27570b045dd3af3d5275c75d18137bc960bd09df
martintufte <martingudahltufte@hotmail.com>
2025-12-14 00:42:38 +0100

    day 6, part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -29,8 +29,57 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(cumulative.iter().sum())
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut it = input.lines().rev();
+
+    // Extract operators
+    let operator_line = it.next();
+    let operators: Vec<&str> = operator_line.unwrap().split_whitespace().collect();
+
+    // Initialize a cumulative sum/product, starting with the default value
+    let mut cumulative: Vec<u64> = operators
+        .iter()
+        .map(|&op| if op == "*" {1} else {0})
+        .collect();
+
+    // Manipulate the order of the text
+    let line_width = operator_line.unwrap().len();
+
+    // Create a grid of chars, filtering out newlines
+    let mut grid: Vec<char> = input.chars().filter(|&b| b != '\n').collect();
+    grid = grid[..grid.len() - line_width].to_vec(); // Drop operator line
+    let n_rows = grid.len() / line_width;
+
+    // Sum up total
+    let mut op_idx: usize = 0;
+    for c in 0..line_width {
+        // Construct string
+        let mut s = String::new();
+        for r in 0..n_rows {
+            s.push(grid[c + line_width * r]);
+
+        }
+
+        // Trim string
+        let trimmed = s.trim();
+
+        // If empty, advance iterator over operators
+        // Else add value to cumulative
+        if trimmed == "" {
+            op_idx += 1;
+        } else {
+            let value: u64 = trimmed.parse().unwrap();
+            if operators[op_idx] == "*" {
+                cumulative[op_idx] *= value;
+            } else {
+                cumulative[op_idx] += value;
+            }
+        }
+
+    }
+
+    // Grand total
+    Some(cumulative.iter().sum())
 }
 
 #[cfg(test)]
@@ -46,6 +95,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&aoc::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3263827));
     }
 }
